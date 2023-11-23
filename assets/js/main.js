6b33903c3ec7a778452ad05c0326faa97b4f0be2
Douglas Rodrigues <123272282+Zatanaris@users.noreply.github.com>
2023-11-23 17:57:15 -0300

    fist commit

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+const RegisterContent = document.querySelector(".container-form");
+const SussesContaint = document.querySelector(".sucess-content");
+const form = document.querySelector(".form");
+const Input = document.querySelector("#Email");
+const error = document.querySelector(".error");
+const Button = document.querySelector(".btn");
+const emailUser = document.querySelector("#user");
+const ResetBtn = document.querySelector("#back");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let InputValue = Input.value;
+
+    if (InputValue === "") {
+        Input.classList.add("error");
+        error.classList.remove("hide");
+    } else {
+        RegisterContent.classList.toggle("hide");
+        SussesContaint.classList.toggle("hide");
+        emailUser.innerHTML = InputValue;
+    }
+});
+
+ResetBtn.addEventListener("click", () => {
+    RegisterContent.classList.toggle("hide");
+    SussesContaint.classList.toggle("hide");
+    Input.value = "";
+});
